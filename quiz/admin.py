4cc13d85158c768061ba59bbfcab714668bc864b
Ayodeji Adeoti <adeoti.15.jude@gmail.com>
2022-09-03 11:10:53 +0100

    Chore: Added models to admin, made DB migrations

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz.models import Answer, Question, Quiz, QuizTaken, UserAnswer
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuizTaken)
+admin.site.register(UserAnswer)
